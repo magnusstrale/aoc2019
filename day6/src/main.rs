b597bdb1f4e3ee722c9e276c55f09f19cabf44bd
Magnus Stråle <magnus.strale@factor10.com>
2019-12-09 22:20:09 +0100

    Solve D6P1 & D6P2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,16 +1,24 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{BufReader, Lines};
+use std::io::BufReader;
 
 mod orbit;
 use orbit::*;
 
 fn main() {
-    println!("Hello, world!");
+    let file_name = "src/day6.txt";
+    let lines = read_file(file_name);
+    let orbits = OrbitMap::new(&(lines.iter().map(|s| &s[..]).collect()));
+
+    let count = orbits.total_orbits();
+    println!("Total orbits {}", count);
+
+    let distance_you_san = orbits.distance();
+    println!("Distance to santa {}", distance_you_san);
 }
 
 
-fn read_file(file_name: &str) -> Lines<BufReader<File>> {
+fn read_file(file_name: &str) -> Vec<String> {
     let file = File::open(file_name).unwrap();
-    BufReader::new(file).lines()
+    BufReader::new(file).lines().map(|l| l.unwrap()).collect()
 }
--- day6/src/orbit.rs
+++ day6/src/orbit.rs
@@ -1,35 +1,59 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Orbit {
     name: String,
-    orbits: String,
-    count: usize
+    center: String
 }
 
 #[derive(Debug, Clone)]
-struct OrbitMap {
+pub struct OrbitMap {
     orbits: HashMap<String, Orbit>
 }
 
 impl OrbitMap {
-    pub  fn new(orbits_input: &[&str]) -> Self {
+    pub  fn new(orbits_input: &Vec<&str>) -> Self {
         let mut orbits = HashMap::new();
         for line in orbits_input {
-            let l = String::from(*line);
-            let mut items = l.split(')');
-            let o = String::from(items.next().unwrap());
-            let n = String::from(items.next().unwrap());
-            let orb = Orbit { name: n.clone(), orbits: o, count: 0 };
-            orbits.insert(n, orb);
+            let items: Vec<&str> = line.split(')').collect();
+            let center = items[0];
+            let name = items[1];
+            let orb = Orbit { name: name.to_string(), center: center.to_string() };
+            orbits.insert(orb.name.clone(), orb);
         }
         OrbitMap {orbits: orbits}
     }
 
-    pub fn total_orbits(&mut self) -> usize {
-        let com = self.orbits.get(&String::from("COM"));
-        println!("{:?}", com);
-        0
+    fn orbits(&self, orbit: &Orbit) -> usize {
+        if orbit.center == "COM".to_string() { return 1; }
+        let new_orbit = self.orbits.get(&orbit.center).unwrap();
+        1 + self.orbits(new_orbit)
+    }
+    
+    pub fn total_orbits(&self) -> usize {
+        self.orbits.values().map(|o| self.orbits(o)).sum()
+    }
+
+    fn orbit_path<'a>(&'a self, orbit: &'a Orbit) -> HashSet<&'a Orbit> {
+        let mut path = HashSet::new();
+        path.insert(orbit);
+        if orbit.center != "COM".to_string() { 
+            let new_orbit = self.orbits.get(&orbit.center).unwrap();
+            path.extend(self.orbit_path(new_orbit));
+        }
+        path
+    }
+
+    fn orbit_path_from_name(&self, name: &str) -> HashSet<&Orbit> {
+        let orbit = self.orbits.get(&name.to_string()).unwrap();
+        self.orbit_path(orbit)
+    }
+
+    pub fn distance(&self) -> usize {
+        let you_set = self.orbit_path_from_name("YOU");
+        let san_set = self.orbit_path_from_name("SAN");
+        let diff: HashSet<_> = you_set.symmetric_difference(&san_set).collect();
+        diff.len() - 2 // Don't cound end nodes, just the path
     }
 }
 
@@ -38,9 +62,21 @@ mod tests {
     use super::*;
 
     #[test]
+    fn sample1_part1_d_orbits_3() {
+        let input = vec!["COM)B", "B)C", "C)D", "D)E","E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L" ];
+        let map = OrbitMap::new(&input);
+
+        let start = map.orbits.get(&"D".to_string()).unwrap();
+        assert_eq!(3, map.orbits(start));
+
+        let start = map.orbits.get(&"L".to_string()).unwrap();
+        assert_eq!(7, map.orbits(start));
+    }
+
+    #[test]
     fn sample1_part1_should_give_42() {
         let input = vec!["COM)B", "B)C", "C)D", "D)E","E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L" ];
-        let mut map = OrbitMap::new(&input);
+        let map = OrbitMap::new(&input);
         println!("{:?}", map);
         assert_eq!(42, map.total_orbits());
     }
@@ -48,7 +84,15 @@ mod tests {
     #[test]
     fn sample1_part1_reordered_should_give_42() {
         let input = vec!["B)C", "C)D", "D)E","E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "COM)B" ];
-        let mut map = OrbitMap::new(&input);
+        let map = OrbitMap::new(&input);
         assert_eq!(42, map.total_orbits());
+    }
+
+    #[test]
+    fn sample1_part2_distance_me_to_santa_is_4() {
+        let input = vec!["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"];
+        let map = OrbitMap::new(&input);
+
+        assert_eq!(4, map.distance());
     }
 }
