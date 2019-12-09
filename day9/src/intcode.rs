cac9229197cf652cf57e2bbfc1ccc9e36bd9381f
Magnus Stråle <magnus.strale@factor10.com>
2019-12-09 16:08:07 +0100

    Add opcode mnemonics and a disassembler

--- day9/src/intcode.rs
+++ day9/src/intcode.rs
@@ -3,6 +3,16 @@ use std::io::prelude::*;
 use std::collections::VecDeque;
 
 pub type Cell = isize;
+const ADD: usize = 1;
+const MULTIPLY: usize = 2;
+const INPUT: usize = 3;
+const OUTPUT: usize = 4;
+const JUMP_NOT_ZERO: usize = 5;
+const JUMP_ZERO: usize = 6;
+const STORE_LESS_THAN: usize = 7;
+const STORE_EQUAL: usize = 8;
+const ADJUST_RELATIVE_BASE: usize = 9;
+const HALT: usize = 99;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum IntCodeState {
@@ -16,12 +26,13 @@ pub struct IntCode {
     pub program: Vec<Cell>,
     pc: usize,
     input: VecDeque<Cell>,
-    relative_base: Cell
+    relative_base: Cell,
+    debug: bool
 }
 
 impl IntCode {
     pub fn new(program: Vec<Cell>) -> Self {
-        IntCode { program, pc: 0, input: VecDeque::new(), relative_base: 0 }
+        IntCode { program, pc: 0, input: VecDeque::new(), relative_base: 0, debug: true }
     }
 
     pub fn file_to_program(file_name: &str) -> Self {
@@ -49,9 +60,8 @@ impl IntCode {
     }
 
     fn allocate(&mut self, absolute_pos: usize) {
-        while self.program.len() <= absolute_pos {
-            self.program.push(0);
-        }
+        let new_len = absolute_pos + 1;
+        if new_len > self.program.len() { self.program.resize(new_len, 0); }
     }
 
     fn peek(&mut self, absolute_pos: usize) -> Cell {
@@ -104,22 +114,23 @@ impl IntCode {
 
     pub fn run_slice(&mut self) -> IntCodeState {
         loop {
+            if self.debug { println!("{}", self.disassemble()); }
             match self.opcode() {
-                1 => { 
+                ADD => { 
                     let p1 = self.p(1); 
                     let p2 = self.p(2); 
                     let p3 = self.p_w(3); 
                     self.poke(p3, p1 + p2); 
                     self.pc += 4;
                 },
-                2 => { 
+                MULTIPLY => { 
                     let p1 = self.p(1); 
                     let p2 = self.p(2); 
                     let p3 = self.p_w(3); 
                     self.poke(p3, p1 * p2); 
                     self.pc += 4; 
                 },
-                3 => match self.input.pop_front() {
+                INPUT => match self.input.pop_front() {
                     None => return IntCodeState::NeedInput,
                     Some(val) => {
                         let p1 = self.p_w(1);
@@ -127,43 +138,127 @@ impl IntCode {
                         self.pc += 2;
                     }
                 },
-                4 => { 
+                OUTPUT => { 
                     let result = IntCodeState::Output(self.p(1)); 
                     self.pc += 2; 
                     return result; 
                 },
-                5 => if self.p(1) != 0 { 
+                JUMP_NOT_ZERO => if self.p(1) != 0 { 
                     self.pc = self.p(2) as usize; 
                 } else { 
                     self.pc += 3 
                 },
-                6 => if self.p(1) == 0 { 
+                JUMP_ZERO => if self.p(1) == 0 { 
                     self.pc = self.p(2) as usize; 
                 } else { 
                     self.pc += 3 
                 },
-                7 => { 
+                STORE_LESS_THAN => { 
                     let p1 = self.p(1); 
                     let p2 = self.p(2); 
                     let p3 = self.p_w(3); 
                     self.bool_poke(p3, p1 < p2); 
                     self.pc += 4; 
                 },
-                8 => { 
+                STORE_EQUAL => { 
                     let p1 = self.p(1); 
                     let p2 = self.p(2); 
                     let p3 = self.p_w(3); 
                     self.bool_poke(p3, p1 == p2); 
                     self.pc += 4; 
                 },
-                9 => {
+                ADJUST_RELATIVE_BASE => {
                     self.relative_base += self.p(1); 
                     self.pc += 2; 
                 }
-                99 => return IntCodeState::Done,
+                HALT => return IntCodeState::Done,
                 _ => panic!("Invalid op-code {} at pc {}", self.opcode(), self.pc)
             }
         }
+    }
+
+    fn disassemble_read_parameter(&mut self, pos: usize) -> String {
+        let immediate = self.peek(self.pc + pos);
+        let result = match self.mode(pos) {
+            0 => format!("[{}] ({})", immediate, self.peek(immediate as usize)),
+            1 => immediate.to_string(),
+            2 =>  {
+                let p = self.peek((immediate + self.relative_base) as usize);
+                format!("[{} + {}] ({})", immediate, self.relative_base, p)
+            },
+            _ => panic!("Invalid parameter mode {}", self.mode(pos))
+        };
+        result
+    }
+
+    fn disassemble_write_parameter(&mut self, pos: usize) -> String {
+        let immediate = self.peek(self.pc + pos);
+        let result = match self.mode(pos) {
+            0 => immediate.to_string(),
+            2 => format!("{}+{} ({})", immediate, self.relative_base, (immediate + self.relative_base) as usize),
+            _ => panic!("Invalid parameter mode for write {}", self.mode(pos))
+        };
+        result
+    }
+
+    fn disassemble(&mut self) -> String {
+        let s = match self.opcode() {
+            ADD => format!("ADD             {}, {} = {} -> {}",
+                    self.disassemble_read_parameter(1), 
+                    self.disassemble_read_parameter(2), 
+                    self.p(1) + self.p(2),
+                    self.disassemble_write_parameter(3)),
+            MULTIPLY => format!("MULTIPLY        {}, {} = {} -> {}", 
+                    self.disassemble_read_parameter(1), 
+                    self.disassemble_read_parameter(2), 
+                    self.p(1) * self.p(2),
+                    self.disassemble_write_parameter(3)),
+            INPUT => match self.input.get(0) {
+                    None => format!("INPUT           NO_DATA -> {} NOP", self.disassemble_read_parameter(1)),
+                    Some(&data) => format!("INPUT           {} -> {}", data, self.disassemble_read_parameter(1))
+                },
+            OUTPUT => format!("OUTPUT          {}", self.disassemble_read_parameter(1)),
+            JUMP_NOT_ZERO => {
+                format!("JUMP_NOT_ZERO   {} = {} TO {}", 
+                    self.disassemble_read_parameter(1),
+                    self.p(1) != 0,
+                    self.disassemble_read_parameter(2))
+            },
+            JUMP_ZERO => {
+                format!("JUMP_ZERO       {} = {} TO {}", 
+                self.disassemble_read_parameter(1), 
+                self.p(1) == 0,
+                self.disassemble_read_parameter(2))
+            },
+            STORE_LESS_THAN => {
+                let p1 = self.p(1); 
+                let p2 = self.p(2); 
+                let result = (p1 < p2) as Cell;
+                format!("STORE_LESS_THAN {}, {} = {} -> {}", 
+                    self.disassemble_read_parameter(1), 
+                    self.disassemble_read_parameter(2),
+                    result,
+                    self.disassemble_write_parameter(3))
+            },
+            STORE_EQUAL => {
+                let p1 = self.p(1); 
+                let p2 = self.p(2); 
+                let result = (p1 == p2) as Cell;
+                format!("STORE_EQUAL     {}, {} = {} -> {}", 
+                    self.disassemble_read_parameter(1), 
+                    self.disassemble_read_parameter(2), 
+                    result, 
+                    self.disassemble_write_parameter(3))
+            },
+            ADJUST_RELATIVE_BASE => {
+                let p1 = self.disassemble_read_parameter(1);
+                let result = self.relative_base + self.p(1);
+                format!("ADJUST          {}, {} = {}", self.relative_base, p1, result) 
+            },
+            HALT => "HALT".to_string(),
+            _ => panic!("Invalid op-code {} at pc {}", self.opcode(), self.pc)
+        };
+        format!("{:5}: {}", self.pc, s)
     }
 }
 
